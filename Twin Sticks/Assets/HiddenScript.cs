8605b56ddededc484ae6d0435b4d7c7543daf640
Mike Madsen <madtufguy@hotmail.com>
2016-07-13 08:43:02 -0600

    Verson control is working check

--- Twin Sticks/Assets/HiddenScript.cs
+++ Twin Sticks/Assets/HiddenScript.cs
@@ -5,7 +5,7 @@ public class HiddenScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        print("Just making a simple change for version control");
 	}
 	
 	// Update is called once per frame
